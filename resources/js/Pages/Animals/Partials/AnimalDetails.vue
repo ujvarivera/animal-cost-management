ae86256c4e563fdc2a5bf93e30a77a84db121afb
ujvarivera <ujvariveronika0320@gmail.com>
2023-10-31 12:55:24 +0100

    components for animal show page

---
+++ resources/js/Pages/Animals/Partials/AnimalDetails.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="p-6 border-b border-gray-200">
+        <div class="flex justify-center items-center my-4">
+            <img v-if="animal.image !== null" :src="showImage() + animal.image" alt="Image about the animal" class="w-80 h-80 rounded-3xl">
+        </div>
+
+        <div class="text-xl flex flex-col justify-center items-center">
+            <p class="py-2">
+                <span class="font-bold">Név: </span>
+                <span>{{ animal.name }}</span>
+            </p>
+            <p class="py-2">
+                <span class="font-bold">Születésnap: </span>
+                <span>{{ animal.birthday }}</span>
+            </p>
+            <p class="py-2">
+                <span class="font-bold">Hímnemű: </span>
+                <span>{{ animal.is_male == 1 ? 'Igen' : 'Nem' }}</span>
+            </p>
+            <p class="py-2">
+                <span class="font-bold">Állatfaj: </span>
+                <span>{{ animal.animal_type.name }}</span>
+            </p>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    animal: Object
+})
+</script>
--- resources/js/Pages/Animals/Show.vue
+++ resources/js/Pages/Animals/Show.vue
@@ -3,131 +3,15 @@
 
     <AuthenticatedLayout>
         <template #header>
-            {{ props.animal.name }} adatai
+            {{ animal.name }} adatai
         </template>
 
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 border-b border-gray-200">
-                <div class="flex justify-center items-center my-4">
-                    <img v-if="animal.image !== null" :src="showImage() + animal.image" alt="Image about the animal" class="w-80 h-80 rounded-3xl">
-                </div>
 
-                <div class="text-xl flex flex-col justify-center items-center">
-                    <p class="py-2">
-                        <span class="font-bold">Név: </span>
-                        <span>{{ animal.name }}</span>
-                    </p>
-                    <p class="py-2">
-                        <span class="font-bold">Születésnap: </span>
-                        <span>{{ animal.birthday }}</span>
-                    </p>
-                    <p class="py-2">
-                        <span class="font-bold">Hímnemű: </span>
-                        <span>{{ animal.is_male == 1 ? 'Igen' : 'Nem' }}</span>
-                    </p>
-                    <p class="py-2">
-                        <span class="font-bold">Állatfaj: </span>
-                        <span>{{ animal.animal_type.name }}</span>
-                    </p>
-                </div>
-
-            </div>
-
-            <div class="p-6 border-b border-gray-200">
-                <DataTable v-model:filters="medicalRecordsFilters" :value="animal.medical_records" paginator :rows="5" 
-                    :rowsPerPageOptions="[5, 10, 20, 50]" sortMode="multiple" removableSort showGridlines 
-                    tableStyle="min-width: 50rem" :globalFilterFields="['description']">
-                    <template #header>
-                        <div class="flex flex-wrap align-items-center justify-content-between gap-2">
-                            <span class="text-xl text-900 font-bold">Vizsgálatok</span>
-                        </div>
-                        <div class="flex justify-end">
-                            <span class="p-input-icon-left">
-                                <TextInput v-model="medicalRecordsFilters['global'].value" placeholder="Keresés" class="w-20"/>
-                            </span>
-                        </div>
-                    </template>
-                    <template #empty> Nem található vizsgálat. </template>
-                    <Column field="description" header="Leírás" sortable></Column>
-                    <Column field="total_cost" header="Kiadás (Forint)" sortable>
-                        <template #body="slotProps">
-                            <div>{{ hufCurrency.format(slotProps.data.total_cost) }}</div>
-                        </template>
-                    </Column>
-                    <Column header="Megtekintés">
-                        <template #body="medicalRecord">
-                            <ButtonLink :href="route('medical-records.show', medicalRecord.data)" class="bg-purple-800 hover:bg-purple-700">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                </svg>
-                            </ButtonLink>
-                        </template>
-                    </Column>
-                    <Column header="Törlés" v-if="isAdmin($page.props.auth.user.role_id)">
-                        <template #body="medicalRecord">
-                            <ButtonLink method="delete" :href="route('medical-records.destroy', medicalRecord.data)" class="bg-red-400 hover:bg-red-500">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                                </svg>
-                            </ButtonLink>
-                        </template>
-                    </Column>
-                    <template #footer> Összes: {{ animal.medical_records ? animal.medical_records.length : 0 }} vizsgálat. </template>
-                </DataTable>
-
-                <DataTable v-model:filters="supplyFilters" :value="animal.supplies" paginator :rows="5" 
-                    :rowsPerPageOptions="[5, 10, 20, 50]" sortMode="multiple" removableSort showGridlines 
-                    tableStyle="min-width: 50rem" :globalFilterFields="['suppliable_type', 'animal.name', 'date', 'suppliable.name', 'suppliable.category']">
-                    <template #header>
-                        <div class="flex flex-wrap align-items-center justify-content-between gap-2">
-                            <span class="text-xl text-900 font-bold">Készletből kapott</span>
-                        </div>
-                        <div class="flex justify-end">
-                            <span class="p-input-icon-left">
-                                <TextInput v-model="supplyFilters['global'].value" placeholder="Keresés" class="w-20"/>
-                            </span>
-                        </div>
-                    </template>
-                    <template #empty> Nem található készlet. </template>
-
-                    <Column field="suppliable_type" header="Típus" sortable>
-                        <template #body="slotProps">
-                            <div>{{ changeSuppliableType(slotProps.data.suppliable_type) }}</div>
-                        </template>
-                    </Column>
-                    <Column field="suppliable.name" header="Termék neve" sortable></Column>             
-                    <Column field="quantity" header="Kapott mennyiség" sortable></Column>
-                    <Column field="date" header="Dátum" sortable></Column>
-                    <Column header="Megtekintés" v-if="false">
-                        <template #body="supply">
-                            <ButtonLink :href="route('supplies.show', supply.data)" class="bg-purple-800 hover:bg-purple-700">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                </svg>
-                            </ButtonLink>
-                        </template>
-                    </Column>
-                    <Column header="Módosítás" v-if="false">
-                        <template #body="supply">
-                            <ButtonLink :href="route('supplies.edit', supply.data)" class="bg-orange-800 hover:bg-orange-700">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                                </svg>
-                            </ButtonLink>
-                        </template>
-                    </Column>
-                    <Column header="Törlés" v-if="isAdmin($page.props.auth.user.role_id)">
-                        <template #body="supply">
-                            <ButtonLink method="delete" :href="route('supplies.destroy', supply.data)" class="bg-red-400 hover:bg-red-500">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                                </svg>
-                            </ButtonLink>
-                        </template>
-                    </Column>
-                </DataTable>
+            <div class="p-6">
+                <AnimalDetails :animal="animal"/>
+                <AnimalMedicalRecordsDatatable :animalMedicalRecords="animal.medical_records"/>
+                <AnimalSupplyDatatable :animalSupplies="animal.supplies"/>
 
             </div>
 
@@ -143,17 +27,14 @@ import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import ButtonLink from '@/Components/Custom/ButtonLink.vue';
 import TextInput from '@/Components/TextInput.vue';
+import AnimalDetails from './Partials/AnimalDetails.vue';
+import AnimalSupplyDatatable from './Partials/AnimalSupplyDatatable.vue';
+import AnimalMedicalRecordsDatatable from './Partials/AnimalMedicalRecordsDatatable.vue';
 import { FilterMatchMode } from 'primevue/api';
 import { ref } from 'vue';
 
 const props = defineProps({
     animal: Object
 })
-const medicalRecordsFilters = ref({
-    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-});
-const supplyFilters = ref({
-    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-});
 
 </script>
